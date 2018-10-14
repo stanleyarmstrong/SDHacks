195789e25d5b654ff70e9436a775981890e1c908
B-Iglesia <bradiglesia@gmail.com>
2018-10-14 01:28:28 -0700

    Button done?

--- gift.io/src/App.js
+++ gift.io/src/App.js
@@ -6,12 +6,28 @@ import Facebook from './Components/facebook';
 //import LowerLimit from './Components/LowerLimit'
 import Price from './Components/Price'
 class App extends Component {
+    state = {
+        fields: {}
+    };
+
+    onChange = updatedValue =>{
+        this.setState({
+            fields:{
+            ...this.state.fields,
+            ...updatedValue
+            }
+        });
+    };
+
   render() {
     return (
       <div className="App">
         <Facebook/>
-        <Price/>
-</div>
+        <Price onChange={fields => this.onChange(fields)}/>
+           <p>
+             {JSON.stringify(this.state.fields, null,2)}
+           </p>
+    </div>
 
     );
   }
--- gift.io/src/Components/Price.jsx
+++ gift.io/src/Components/Price.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default class Price extends React.Component {
     state = {
-        Lower: parseInt('10',10),
-        Upper: parseInt('100',10)
-    }
+        Lower: "",
+        Upper: ""
+    };
     change = e =>{
+        this.props.onChange({[e.target.value]: e.target.value});
         this.setState({
             [e.target.name]: e.target.value
 
@@ -13,8 +14,16 @@ export default class Price extends React.Component {
     };
     onSubmit = e => {
         e.preventDefault();
-        this.props.onSubmit(this.state);
+        //this.props.onSubmit(this.state);
         console.log(this.state);
+        this.setState({
+            Lower: "",
+            Upper: ""
+        });
+        this.props.onChange({
+            Lower: "",
+            Upper: ""
+        });
     };
     render() {
         return (
